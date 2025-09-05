593404ba92fb3bf1a31a50a0bbd340d8f3d24bfc
Alex Cohen <alexcohen.inbox@gmail.com>
2025-09-04 21:55:58 -0400

    refactor: immediate ingestion scheduling during rag_opt backfill

--- gregg_limper/config/cache.py
+++ gregg_limper/config/cache.py
@@ -4,7 +4,7 @@ import os
 
 @dataclass
 class Cache:
-    CACHE_LENGTH: int = int(os.getenv("CACHE_LENGTH", "1000"))
+    CACHE_LENGTH: int = int(os.getenv("CACHE_LENGTH", "200"))
     MEMO_DIR: str = os.getenv("MEMO_DIR", "gregg_limper/memory/cache/data")
     INIT_CONCURRENCY: int = int(os.getenv("CACHE_INIT_CONCURRENCY", "20"))
     INGEST_CONCURRENCY: int = int(os.getenv("CACHE_INGEST_CONCURRENCY", "20"))
